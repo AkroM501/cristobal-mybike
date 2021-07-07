51dfc151f927713528fabb9b079d93734fda1c8c
AkroM501 <chunchito1290@gmail.com>
2021-07-07 19:50:46 -0400

    cambiando uri api backend

--- client/src/config/Axios.ts
+++ client/src/config/Axios.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 //const backEnd_API = "http://localhost:4000";
-const backEnd_API = "https://shop-akrom-be.vercel.app";
+const backEnd_API = "https://cristobal-mybike-backend.vercel.app";
 
 export default Axios.create({
     baseURL: backEnd_API,
